39f511182082521f26d06bd817b81ad623526a40
Anuj Lahoty <anujlahoty1997@gmail.com>
2022-05-03 23:10:45 +0530

    Added the user actions

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -6,8 +6,10 @@ from watchlist_app.api.views import (WatchListAV, WatchDetailAV,
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>/', WatchDetailAV.as_view(), name='movie-detail'),
+    
     path('stream/', StreamPlatformListAV.as_view(), name='stream-list'),
     path('stream/<int:pk>/', StreamPlatformDetailAV.as_view(), name='stream-detail'),
+    
     path('<int:pk>/reviews/', ReviewsList.as_view(), name='review-list'),
     path('<int:pk>/review-create/', ReviewsCreate.as_view(), name='review-create'),
     path('reviews/<int:pk>/', ReviewsDetail.as_view(), name='review-detail'),
